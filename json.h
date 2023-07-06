1bdc3c2b13cf2fa1daa01e135bf1f66dde1ac97e
skyue <104967297+Skyue1@users.noreply.github.com>
2023-07-06 17:49:57 +0900

    Add custom indent, better json function

--- json.h
+++ json.h
@@ -3,89 +3,78 @@
 #include <vector>
 #include <fstream>
 
-namespace json {
+namespace skyue {
 
-    typedef std::map<std::string, std::string> JSONKeyValuePair;
-    typedef std::vector<std::string> JSONArray;
+    namespace jsonh {
 
-    struct JSONObject {
-        JSONKeyValuePair pairs;
-    };
+        // Define some aliases for convenience
+        typedef std::map<std::string, std::string> JSONKeyValuePair;
+        typedef std::vector<std::string> JSONArray;
 
-    JSONObject createJSONObject() {
-        JSONObject object;
-        return object;
+        // Define a struct for a JSON object
+        struct JSONObject {
+            JSONKeyValuePair pairs;
+        };
+
+        // Define a class for a JSON object
+        class JSON {
+        private:
+            JSONObject object;
+            int indent_spaces = 0;
+
+        public:
+            // Constructor
+            JSON() {}
+
+            // Overload the [] operator to allow access to key-value pairs
+            std::string& operator[](const std::string& key) {
+                return object.pairs[key];
+            }
+
+            // Convert the JSON object to a string
+            std::string json_to_string() const {
+                std::string indent(indent_spaces, ' ');
+                std::string result = "{\n";
+                for (const auto& pair : object.pairs) {
+                    result += indent + "\"" + pair.first + "\": \"" + pair.second + "\",\n";
+                }
+                if (!object.pairs.empty()) {
+                    result.pop_back();
+                    result.pop_back();
+                }
+                result += "\n}";
+                return result;
+            }
+
+            // Save the JSON object to a file
+            void json_to_file(const std::string& filename) const {
+                std::ofstream file(filename);
+
+                if (file.is_open()) {
+                    std::string indent(indent_spaces, ' ');
+                    file << "{\n";
+                    for (const auto& pair : object.pairs) {
+                        file << indent << "\"" << pair.first << "\": \"" << pair.second << "\",\n";
+                    }
+                    if (!object.pairs.empty()) {
+                        file.seekp(-2, std::ios_base::end);
+                        file << "\n";
+                    }
+                    file << "}";
+                    file.close();
+                }
+            }
+
+            // Set the number of spaces for indentation
+            void indent(int spaces) {
+                indent_spaces = spaces;
+            }
+
+            // Create a JSON array
+            JSONArray create_json_array() {
+                JSONArray array;
+                return array;
+            }
+        };
     }
-
-    void addKeyValuePair(JSONObject& object, const std::string& key, const std::string& value) {
-        object.pairs[key] = value;
-    }
-
-    std::string toJSONString(const JSONObject& object) {
-        std::string result = "{\n";
-        for (const auto& pair : object.pairs) {
-            result += "  \"" + pair.first + "\": \"" + pair.second + "\",\n";
-        }
-        if (!object.pairs.empty()) {
-            result.pop_back();
-            result.pop_back();
-        }
-        result += "\n}";
-        return result;
-    }
-
-    void printJSONObject(const JSONObject& object) {
-        std::cout << toJSONString(object) << std::endl;
-    }
-
-    void saveJSONObjectToFile(const JSONObject& object, const std::string& filename) {
-        std::ofstream file(filename);
-        if (file.is_open()) {
-            file << toJSONString(object);
-            file.close();
-            std::cout << "JSON object saved to file: " << filename << std::endl;
-        }
-        else {
-            std::cout << "Failed to open file for saving." << std::endl;
-        }
-    }
-
-    JSONArray createJSONArray() {
-        JSONArray array;
-        return array;
-    }
-
-    void addElement(JSONArray& array, const std::string& value) {
-        array.push_back(value);
-    }
-
-    std::string toJSONString(const JSONArray& array) {
-        std::string result = "[\n";
-        for (const auto& element : array) {
-            result += "  \"" + element + "\",\n";
-        }
-        if (!array.empty()) {
-            result.pop_back();
-            result.pop_back();
-        }
-        result += "\n]";
-        return result;
-    }
-
-    void printJSONArray(const JSONArray& array) {
-        std::cout << toJSONString(array) << std::endl;
-    }
-
-    void saveJSONArrayToFile(const JSONArray& array, const std::string& filename) {
-        std::ofstream file(filename);
-        if (file.is_open()) {
-            file << toJSONString(array);
-            file.close();
-            std::cout << "JSON array saved to file: " << filename << std::endl;
-        }
-        else {
-            std::cout << "Failed to open file for saving." << std::endl;
-        }
-    }
-
 }
